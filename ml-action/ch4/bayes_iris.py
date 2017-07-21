6cef635452a92816394646119d474e71f723aab0
javasaloon <calm2016@yeah.net>
2017-07-22 07:55:25 +0800

    refact

--- ml-action/ch4/bayes_iris.py
+++ ml-action/ch4/bayes_iris.py
@@ -1,18 +1,27 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
-iris = load_iris()
-x, y = iris.data[:, :2], iris.target
 
-x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.8, random_state=1)
+def train_nb(x_train, y_train):
+    print [x_train[(y_train == i)].shape for i in range(3)]
+    mu = [x_train[(y_train == i)].mean(axis=0) for i in range(3)]
 
-print x.shape, x_train.shape, x_valid.shape
+    sigma = [((x_train[(y_train == i)] - mu[i]) ** 2).mean(axis=0) for i in range(3)]
+    print mu
+    print sigma
 
-print [x_train[(y_train == i)].shape for i in range(3)]
-mu = [x_train[(y_train == i)].mean(axis=0) for i in range(3)]
-
-sigma = [((x_train[(y_train == i)] - mu[i]) ** 2).mean(axis=0) for i in range(3)]
-print mu
-print sigma
+    return mu, sigma
 
 
+def main():
+    iris = load_iris()
+    x, y = iris.data[:, :2], iris.target
+
+    x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.8, random_state=1)
+    print x.shape, x_train.shape, x_valid.shape
+
+    train_nb(x_train, y_train)
+
+
+if __name__ == "__main__":
+    main()
