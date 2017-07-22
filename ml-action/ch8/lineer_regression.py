ed1b1f2ad61965d3da0c2abe3cf092cbd94d26b8
javasaloon <calm2016@yeah.net>
2017-07-23 07:37:18 +0800

    lineer regression standard regression done

--- ml-action/ch8/lineer_regression.py
+++ ml-action/ch8/lineer_regression.py
@@ -2,8 +2,30 @@ import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pandas.read_csv('ex0.csv', sep='\t', header=None)
-print data.shape
-plt.scatter(data[1], data[2])
-plt.show()
 
+def main():
+    data = pandas.read_csv('ex0.csv', sep='\t', header=None)
+    print data.shape
+
+    plt.scatter(data[1], data[2])
+
+    X = np.mat(data[[0, 1]].values)
+    y = np.mat(data[2].values)
+
+    print X.shape, y.shape
+    xTx = X.T * X
+    print xTx
+
+    if np.linalg.det(xTx) == 0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    ws = xTx.I * (X.T * y.T)
+    print ws
+    y_hat = X * ws
+    plt.plot(data[1], y_hat, 'r')
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
