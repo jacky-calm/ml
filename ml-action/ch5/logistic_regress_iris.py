087f575416421b42da7af0cf9a38dc7fb9f6f111
javasaloon <calm2016@yeah.net>
2017-07-22 16:39:04 +0800

    training done, plot w and error trace

--- ml-action/ch5/logistic_regress_iris.py
+++ ml-action/ch5/logistic_regress_iris.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(z):
@@ -8,15 +9,35 @@ def sigmoid(z):
 
 
 data, target = load_iris(True)
+data, target = data[target != 2], target[target != 2] # only choose two classes of iris
+
+data = np.append(np.ones((data.shape[0], 1)), data, axis=1)
 
 x_train, x_test, y_train, y_test = train_test_split(data, target, train_size=0.8, random_state=1)
+
+print x_train.shape
+
 weights = np.ones(data.shape[1])
 alpha = 0.1
 
-for i in range(x_train.shape[0]):
-    pick = x_train[i]
+w_trace, h_trace, error_trace, y_train_trace = [], [], [], []
+for i in range(200):
+    random_i = int(np.random.uniform(0, x_train.shape[0]))
+    pick = x_train[random_i]
     h = sigmoid(np.dot(weights, pick))
-    error = h - y_train[i]
+    h_trace.append(h)
+    error = h - y_train[random_i]
+    y_train_trace.append(y_train[random_i])
+    error_trace.append(error)
     weights = weights - alpha * error * pick
+    w_trace.append(weights)
 
-    print weights
+plt.subplot(311)
+plt.plot(w_trace)
+plt.title('weights')
+
+plt.subplot(312)
+plt.plot(error_trace)
+plt.title('errors')
+
+plt.show()
