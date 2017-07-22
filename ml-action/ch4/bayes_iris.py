7207e05fdaf4eef35a262e0a61b330e323c2b71a
javasaloon <calm2016@yeah.net>
2017-07-22 09:46:51 +0800

    done: x0, x1, accuracy:  0 1 0.833333333333 x0, x1, accuracy:  0 2 0.966666666667 x0, x1, accuracy:  0 3 0.933333333333 x0, x1, accuracy:  1 2 0.933333333333 x0, x1, accuracy:  1 3 0.933333333333 x0, x1, accuracy:  2 3 0.966666666667

--- ml-action/ch4/bayes_iris.py
+++ ml-action/ch4/bayes_iris.py
@@ -4,39 +4,48 @@ import numpy as np
 
 
 def std(x, mu):
-    return np.sqrt(((x-mu) ** 2).mean(axis=0))
+    return np.sqrt(((x - mu) ** 2).mean(axis=0))
 
 
 def gaussian(x, mu, sigma):
-    print x, mu, sigma
-    print x - mu, np.square(x - mu)
     return 1 / np.sqrt(2 * np.pi) * np.exp(-np.square(x - mu) / (2 * np.square(sigma)))
 
 
 def train_nb(x_train, y_train):
-    print [x_train[(y_train == i)].shape for i in range(3)]
+    # print [x_train[(y_train == i)].shape for i in range(3)]
     mu = [x_train[(y_train == i)].mean(axis=0) for i in range(3)]
     sigma = [std(x_train[(y_train == i)], mu[i]) for i in range(3)]
-    sigma2 = [np.std(x_train[(y_train == i)], axis=0) for i in range(3)]
-    print "sigma, sigma2: ===== "
-    print sigma
-    print sigma2
-    print "sigma, sigma2: ===== "
+    # sigma2 = [np.std(x_train[(y_train == i)], axis=0) for i in range(3)]
+    # print "sigma, sigma2: ===== "
+    # print sigma
+    # print sigma2
+    # print "sigma, sigma2: ===== "
     return mu, sigma
+
+
+def classify(x, mu, sigma, pOc):
+    p = [np.log(gaussian(x, mu[i], sigma[i])).sum() + pOc[i] for i in range(3)]
+    # print "p of c given x ===== ", p, np.argmax(p)
+    return np.argmax(p)
 
 
 def main():
     iris = load_iris()
-    x, y = iris.data[:, :2], iris.target
+    for x0 in range(3):
+        for x1 in range(x0+1, 4):
+            x, y = iris.data[:, (x0, x1)], iris.target
 
-    x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.8, random_state=1)
-    print x.shape, x_train.shape, x_valid.shape
+            x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.8, random_state=1)
+            # print x.shape, x_train.shape, x_valid.shape
 
-    mu, sigma = train_nb(x_train, y_train)
-    print mu
-    print sigma
-    p = [gaussian(x_valid[0], mu[i], sigma[i]) for i in range(3)]
-    print "gaussian ===== ", p
+            mu, sigma = train_nb(x_train, y_train)
+            # print "mu: ", mu
+            # print "sigma: ", sigma
+
+            pOc = [(y_train == i).mean() for i in range(3)]
+            # print "pOc ====== ", pOc
+            y_predict = [classify(x, mu, sigma, pOc) for x in x_valid]
+            print "x0, x1, accuracy: ", x0, x1, (y_predict == y_valid).mean()
 
 
 if __name__ == "__main__":
