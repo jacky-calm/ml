80c641c468cdcb4fdd813947feaef3ca64eb90a1
javasaloon <calm2016@yeah.net>
2017-07-22 11:21:56 +0800

    plot is done

--- ml-action/ch4/bayes_iris.py
+++ ml-action/ch4/bayes_iris.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def std(x, mu):
@@ -29,14 +30,16 @@ def classify(x, mu, sigma, pOc):
     return np.argmax(p)
 
 
+
 def main():
     iris = load_iris()
+    feature_names = ['sepal length', 'sepal width', 'petal length', 'petal width']
     for x0 in range(3):
         for x1 in range(x0+1, 4):
             x, y = iris.data[:, (x0, x1)], iris.target
 
             x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.8, random_state=1)
-            # print x.shape, x_train.shape, x_valid.shape
+            print x.shape, x_train.shape, x_valid.shape
 
             mu, sigma = train_nb(x_train, y_train)
             # print "mu: ", mu
@@ -44,9 +47,21 @@ def main():
 
             pOc = [(y_train == i).mean() for i in range(3)]
             # print "pOc ====== ", pOc
-            y_predict = [classify(x, mu, sigma, pOc) for x in x_valid]
-            print "x0, x1, accuracy: ", x0, x1, (y_predict == y_valid).mean()
+            y_predict = [classify(v, mu, sigma, pOc) for v in x_valid]
+            accuracy = (y_predict == y_valid).mean()
 
+            error = x_valid[y_predict != y_valid]
+            print "x0, x1, accuracy: ", accuracy
+            if x0 == 0:
+                plt.subplot(3, 1, x1)
+                plt.scatter(x_train[:, 0], x_train[:, 1], c=y_train+10, marker=".")
+                plt.scatter(x_valid[:, 0], x_valid[:, 1], c=y_valid+100, marker="^")
+                plt.scatter(error[:, 0], error[:, 1], c='r', marker="o")
+                plt.xlabel(feature_names[x0])
+                plt.ylabel(feature_names[x1])
+                plt.title("accuracy {}".format(accuracy))
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
