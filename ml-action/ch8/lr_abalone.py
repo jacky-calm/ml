79f5360a075299728dbfefe97cee3677bd8deb82
javasaloon <calm2016@yeah.net>
2017-07-23 13:23:07 +0800

    lineer regression test error

--- ml-action/ch8/lr_abalone.py
+++ ml-action/ch8/lr_abalone.py
@@ -8,5 +8,5 @@ print data.shape
 
 X, y = data.values[0:99, 0:-1], data.values[0:99, -1]
 print X.shape, y.shape
-y_hat = linear_regression.lwlr_test(X, X, y, 0.1)
-print y_hat
+y_hat = linear_regression.lwlr_test(data.values[100:199, 0:-1], X, y, 0.1)
+print np.square(y_hat - data.values[100:199, -1]).sum()
